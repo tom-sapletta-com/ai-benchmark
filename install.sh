437ed2ddd6155fe7b7b3368228697f9221b709a3
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2023-05-27 12:24:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+sudo apt install python3-dev python3-pip
+#sudo dnf install python3-dev python3-pip
 python --version
+pip install --upgrade pip
+# install tensorflow from python packages
 pip install tensorflow
+pip install tensorflow-cpu
 pip install ai-benchmark
+pip install tf-nightly
+# install tensorflow from source
+git clone https://github.com/tensorflow/tensorflow
+cd tensorflow
+git checkout r2.8
+# https://bazel.build/install
+sudo dnf -y install dnf-plugins-core
+sudo dnf -y copr enable vbatts/bazel
+sudo dnf -y install bazel4
+# config
+./configure
